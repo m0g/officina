8218808040759fc5e6f1c76320742f9e8b0a9ab5
Loïc Nogues <nogues.loic@gmail.com>
2020-03-05 12:20:40 +0100

    Handle copy

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/admin");
+
+  return {
+    // templateFormats: ["md", "html", "liquid"],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    // pathPrefix: "/",
+
+    // markdownTemplateEngine: "md",
+    // htmlTemplateEngine: "liquid",
+    // includesdataTemplateEngine: "njk",
+    // passthroughFileCopy: true,
+    dir: {
+      input: "src/",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+}

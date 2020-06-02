d015b6dcc8ad0a7936aefaaf66b358ae79a1c7c2
Loïc Nogues <nogues.loic@gmail.com>
2020-06-02 15:08:18 +0200

    Image processing

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const pluginLocalRespimg = require('eleventy-plugin-local-respimg');
 const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 const { DateTime } = require('luxon');
 
@@ -6,18 +7,59 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_assets");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
 
-  eleventyConfig.addPlugin(lazyImagesPlugin, {
-    imgSelector: 'img.lazy',
-    transformImgPath: imagePath => {
-      const match = imagePath.match(/(_assets.+)/);
-
-      if (!match) {
-        return imagePath;
-      }
-
-      return `./src/${match[0]}`;
-    }
+  eleventyConfig.addPlugin(pluginLocalRespimg, {
+    folders: {
+      source: 'src', // Folder images are stored in
+      output: '_site', // Folder images should be output to
+    },
+    images: {
+      resize: {
+        min: 250, // Minimum width to resize an image to
+        max: 1500, // Maximum width to resize an image to
+        step: 150, // Width difference between each resized image
+      },
+      gifToVideo: false, // Convert GIFs to MP4 videos
+      sizes: '100vw', // Default image `sizes` attribute
+      lazy: true, // Include `loading="lazy"` attribute for images
+      // additional: [
+      //   'images/icons/**/*',
+      // ],
+      // watch: {
+      //   src: 'images/**/*', // Glob of images that Eleventy should watch for changes to
+      // },
+      pngquant: {
+        /* ... */
+      }, // imagemin-pngquant options
+      mozjpeg: {
+        /* ... */
+      }, // imagemin-mozjpeg options
+      svgo: {
+        /* ... */
+      }, // imagemin-svgo options
+      gifresize: {
+        /* ... */
+      }, // @gumlet/gif-resize options
+      webp: {
+        /* ... */
+      }, // imagemin-webp options
+      gifwebp: {
+        /* ... */
+      }, // imagemin-gif2webp options
+    },
   });
+
+  // eleventyConfig.addPlugin(lazyImagesPlugin, {
+  //   imgSelector: 'img.lazy',
+  //   transformImgPath: imagePath => {
+  //     const match = imagePath.match(/(_assets.+)/);
+
+  //     if (!match) {
+  //       return imagePath;
+  //     }
+
+  //     return `./src/${match[0]}`;
+  //   }
+  // });
 
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');

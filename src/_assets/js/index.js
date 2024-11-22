c724805a886ecef947d49ffe820c617c0f42e65d
Loïc Nogues <nogues.loic@gmail.com>
2024-11-22 15:44:36 +0100

    Remove popup

--- src/_assets/js/index.js
+++ src/_assets/js/index.js
@@ -1,7 +1,5 @@
 import './htmx.js';
 import { map as lMap, tileLayer, marker, Browser } from './leaflet.js';
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.13.1/cdn/components/dialog/dialog.js';
-import 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.13.1/cdn/components/button/button.js';
 
 // MAP
 document.body.addEventListener('htmx:load', function () {
@@ -22,19 +20,6 @@ document.body.addEventListener('htmx:load', function () {
     }).addTo(map);
 
     marker(position).addTo(map).bindPopup('Officina').openPopup();
-  }
-
-  const popup = document.getElementById('popup');
-
-  if (!document.cookie.match(/shown_popup=1/) && popup) {
-    const closeButton = popup.querySelector('sl-button[slot="footer"]');
-
-    popup.show();
-    closeButton.addEventListener('click', () => popup.hide());
-
-    popup.addEventListener('sl-after-hide', () => {
-      document.cookie = 'shown_popup=1; SameSite=Strict';
-    });
   }
 });
 
